462ac82564dccb9887e2d32e6c91148b595f5eec
Mir Farid <mirfarid7160@gmail.com>
2020-11-26 15:37:10 -0500

    "initial commit"

--- Parsestagram/AppDelegate.swift
+++ Parsestagram/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Mir Farid on 11/24/20.
 //
-
+import Parse
 import UIKit
 
 @main
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "FfBuux4zQu7qejXSS86l6Qy6aHliPHWmpVJ6Kyo4" // <- UPDATE
+                    $0.clientKey = "khQfZkezo0zu8kOjC05g9wzaQ6jFHRTbAQQknZZB" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
